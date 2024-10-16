97774f15b1f4086cfd7d7e1f0c60bf10d0f1d226
peke <152220956+peke7374@users.noreply.github.com>
2024-10-15 22:05:09 -0300

    Update Skyboxes.lua

--- Deps/Skyboxes.lua
+++ Deps/Skyboxes.lua
@@ -259,6 +259,285 @@ local Skyboxes = {
         SkyboxRt = "rbxassetid://433381097",
         SkyboxUp = "rbxassetid://433381097",
     },
+
+    ClearDay = {
+        SkyboxBk = "rbxassetid://591058823",
+        SkyboxDn = "rbxassetid://591059876",
+        SkyboxFt = "rbxassetid://591058104",
+        SkyboxLf = "rbxassetid://591057861",
+        SkyboxRt = "rbxassetid://591057625",
+        SkyboxUp = "rbxassetid://591059642",
+    },
+
+    Mountains = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=324014980",
+        SkyboxDn = "http://www.roblox.com/asset/?id=324015477",
+        SkyboxFt = "http://www.roblox.com/asset/?id=324014995",
+        SkyboxLf = "http://www.roblox.com/asset/?id=324014679",
+        SkyboxRt = "http://www.roblox.com/asset/?id=324015013",
+        SkyboxUp = "http://www.roblox.com/asset/?id=324015409",
+    },
+
+    Forest = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=70945545",
+        SkyboxDn = "http://www.roblox.com/asset/?id=70945449",
+        SkyboxFt = "http://www.roblox.com/asset/?id=70945487",
+        SkyboxLf = "http://www.roblox.com/asset/?id=70945523",
+        SkyboxRt = "http://www.roblox.com/asset/?id=70945508",
+        SkyboxUp = "http://www.roblox.com/asset/?id=70945531",
+    },
+
+    LargeForest = {
+        SkyboxBk = "rbxassetid://17428978603",
+        SkyboxDn = "rbxassetid://17428977445",
+        SkyboxFt = "rbxassetid://17428977114",
+        SkyboxLf = "rbxassetid://17428978399",
+        SkyboxRt = "rbxassetid://17428976828",
+        SkyboxUp = "rbxassetid://17428976669",
+    },
+
+    Crimson = {
+        SkyboxBk = "rbxassetid://15832429892",
+        SkyboxDn = "rbxassetid://15832430998",
+        SkyboxFt = "rbxassetid://15832430210",
+        SkyboxLf = "rbxassetid://15832430671",
+        SkyboxRt = "rbxassetid://15832431198",
+        SkyboxUp = "rbxassetid://15832429401",
+    },
+
+    PumpkinHill = {
+        SkyboxBk = "rbxassetid://11202510597",
+        SkyboxDn = "rbxassetid://11202510255",
+        SkyboxFt = "rbxassetid://11202509993",
+        SkyboxLf = "rbxassetid://11202510806",
+        SkyboxRt = "rbxassetid://11202511066",
+        SkyboxUp = "rbxassetid://11202509704",
+    },
+
+    AnimeIsland = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=14753804949",
+        SkyboxDn = "http://www.roblox.com/asset/?id=14753795573",
+        SkyboxFt = "http://www.roblox.com/asset/?id=14753807625",
+        SkyboxLf = "http://www.roblox.com/asset/?id=14753797417",
+        SkyboxRt = "http://www.roblox.com/asset/?id=14753799966",
+        SkyboxUp = "http://www.roblox.com/asset/?id=14753810287",
+    },
+
+    SnowyMountains = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=368385273",
+        SkyboxDn = "http://www.roblox.com/asset/?id=48015300",
+        SkyboxFt = "http://www.roblox.com/asset/?id=368388290",
+        SkyboxLf = "http://www.roblox.com/asset/?id=368390615",
+        SkyboxRt = "http://www.roblox.com/asset/?id=368385190",
+        SkyboxUp = "http://www.roblox.com/asset/?id=48015387",
+    },
+
+    Desert = {
+        SkyboxBk = "rbxassetid://161319957",
+        SkyboxDn = "rbxassetid://161319965",
+        SkyboxFt = "rbxassetid://161319970",
+        SkyboxLf = "rbxassetid://161319983",
+        SkyboxRt = "rbxassetid://161319989",
+        SkyboxUp = "rbxassetid://161319996",
+    },
+
+    Cloudy = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=225469345",
+        SkyboxDn = "http://www.roblox.com/asset/?id=225469349",
+        SkyboxFt = "http://www.roblox.com/asset/?id=225469359",
+        SkyboxLf = "http://www.roblox.com/asset/?id=225469364",
+        SkyboxRt = "http://www.roblox.com/asset/?id=225469372",
+        SkyboxUp = "http://www.roblox.com/asset/?id=225469380",
+    },
+
+    Island = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=319343577",
+        SkyboxDn = "http://www.roblox.com/asset/?id=319343653",
+        SkyboxFt = "http://www.roblox.com/asset/?id=319343666",
+        SkyboxLf = "http://www.roblox.com/asset/?id=319343686",
+        SkyboxRt = "http://www.roblox.com/asset/?id=319343631",
+        SkyboxUp = "http://www.roblox.com/asset/?id=319343614",
+    },
+
+    OrangeFog = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=458016711",
+        SkyboxDn = "http://www.roblox.com/asset/?id=458016826",
+        SkyboxFt = "http://www.roblox.com/asset/?id=458016532",
+        SkyboxLf = "http://www.roblox.com/asset/?id=458016655",
+        SkyboxRt = "http://www.roblox.com/asset/?id=458016782",
+        SkyboxUp = "http://www.roblox.com/asset/?id=458016792",
+    },
+
+    FadeNight = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=16888843486",
+        SkyboxDn = "http://www.roblox.com/asset/?id=16888845693",
+        SkyboxFt = "http://www.roblox.com/asset/?id=16888848245",
+        SkyboxLf = "http://www.roblox.com/asset/?id=16888850949",
+        SkyboxRt = "http://www.roblox.com/asset/?id=16888854243",
+        SkyboxUp = "http://www.roblox.com/asset/?id=16888857144",
+    },
+
+    Office = {
+        SkyboxBk = "rbxassetid://658623433",
+        SkyboxDn = "rbxassetid://316342560",
+        SkyboxFt = "rbxassetid://658625205",
+        SkyboxLf = "rbxassetid://658627155",
+        SkyboxRt = "rbxassetid://658628504",
+        SkyboxUp = "rbxassetid://658632701",
+    },
+
+    Spongebob2 = {
+        SkyboxBk = "rbxassetid://12049872454",
+        SkyboxDn = "rbxassetid://12049872284",
+        SkyboxFt = "rbxassetid://12049872181",
+        SkyboxLf = "rbxassetid://12049872074",
+        SkyboxRt = "rbxassetid://12049871884",
+        SkyboxUp = "rbxassetid://12049871774",
+    },
+
+    PurpleFog = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=17279854976",
+        SkyboxDn = "http://www.roblox.com/asset/?id=17279856318",
+        SkyboxFt = "http://www.roblox.com/asset/?id=17279858447",
+        SkyboxLf = "http://www.roblox.com/asset/?id=17279860360",
+        SkyboxRt = "http://www.roblox.com/asset/?id=17279862234",
+        SkyboxUp = "http://www.roblox.com/asset/?id=17279864507",
+    },
+
+    EarthSpace = {
+        SkyboxBk = "rbxassetid://15753305495",
+        SkyboxDn = "rbxassetid://15753362674",
+        SkyboxFt = "rbxassetid://15753305823",
+        SkyboxLf = "rbxassetid://15753310707",
+        SkyboxRt = "rbxassetid://15753304774",
+        SkyboxUp = "rbxassetid://15753304473",
+    },
+    
+    GreenCloudy = {
+        SkyboxBk = "rbxassetid://921882045",
+        SkyboxDn = "rbxassetid://921881907",
+        SkyboxFt = "rbxassetid://921882121",
+        SkyboxLf = "rbxassetid://921881811",
+        SkyboxRt = "rbxassetid://921881989",
+        SkyboxUp = "rbxassetid://921882259",
+    },
+
+    SummerDay = {
+        SkyboxBk = "http://www.roblox.com/asset/?version=1&id=135483466",
+        SkyboxDn = "http://www.roblox.com/asset/?version=1&id=135483484",
+        SkyboxFt = "http://www.roblox.com/asset/?version=1&id=135483461",
+        SkyboxLf = "http://www.roblox.com/asset/?version=1&id=135483495",
+        SkyboxRt = "http://www.roblox.com/asset/?version=1&id=135483499",
+        SkyboxUp = "http://www.roblox.com/asset/?version=1&id=135483475",
+    },
+
+    SnowyPlains = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=155657655",
+        SkyboxDn = "http://www.roblox.com/asset/?id=155674246",
+        SkyboxFt = "http://www.roblox.com/asset/?id=155657609",
+        SkyboxLf = "http://www.roblox.com/asset/?id=155657671",
+        SkyboxRt = "http://www.roblox.com/asset/?id=155657619",
+        SkyboxUp = "http://www.roblox.com/asset/?id=155674931",
+    },
+
+    Underwater = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=227635868",
+        SkyboxDn = "http://www.roblox.com/asset/?id=227635921",
+        SkyboxFt = "http://www.roblox.com/asset/?id=227635954",
+        SkyboxLf = "http://www.roblox.com/asset/?id=227635974",
+        SkyboxRt = "http://www.roblox.com/asset/?id=227635990",
+        SkyboxUp = "http://www.roblox.com/asset/?id=227636031",
+    }
+
+    BlueAbyss = {
+        SkyboxBk = "rbxassetid://16269815885",
+        SkyboxDn = "rbxassetid://16269839652",
+        SkyboxFt = "rbxassetid://16269798011",
+        SkyboxLf = "rbxassetid://16269813852",
+        SkyboxRt = "rbxassetid://16269814948",
+        SkyboxUp = "rbxassetid://16269829700",
+    },
+
+    Poison = {
+        SkyboxBk = "rbxassetid://1370716695",
+        SkyboxDn = "rbxassetid://1370716766",
+        SkyboxFt = "rbxassetid://1370716833",
+        SkyboxLf = "rbxassetid://1370716898",
+        SkyboxRt = "rbxassetid://1370716955",
+        SkyboxUp = "rbxassetid://1370717024",
+    },
+
+    BlueSpace = {
+        SkyboxBk = "rbxassetid://1127563035",
+        SkyboxDn = "rbxassetid://1127563006",
+        SkyboxFt = "rbxassetid://1127563026",
+        SkyboxLf = "rbxassetid://1127563216",
+        SkyboxRt = "rbxassetid://1127563115",
+        SkyboxUp = "rbxassetid://1127562999",
+    },
+
+    AnimeMountains = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=12849370744",
+        SkyboxDn = "http://www.roblox.com/asset/?id=12849378890",
+        SkyboxFt = "http://www.roblox.com/asset/?id=12849390276",
+        SkyboxLf = "http://www.roblox.com/asset/?id=12849405549",
+        SkyboxRt = "http://www.roblox.com/asset/?id=12849398428",
+        SkyboxUp = "http://www.roblox.com/asset/?id=12849426002",
+    },
+
+    PinkGradient = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=5371541816",
+        SkyboxDn = "http://www.roblox.com/asset/?id=5371541154",
+        SkyboxFt = "http://www.roblox.com/asset/?id=5371541816",
+        SkyboxLf = "http://www.roblox.com/asset/?id=5371541816",
+        SkyboxRt = "http://www.roblox.com/asset/?id=5371541816",
+        SkyboxUp = "http://www.roblox.com/asset/?id=5371540604",
+    },
+
+    YellowGradient = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=159005370",
+        SkyboxDn = "rbxassetid://858422412",
+        SkyboxFt = "http://www.roblox.com/asset/?id=159005370",
+        SkyboxLf = "http://www.roblox.com/asset/?id=159005370",
+        SkyboxRt = "http://www.roblox.com/asset/?id=159005370",
+        SkyboxUp = "http://www.roblox.com/asset/?id=159006363",
+    },
+
+    BlueGradient = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=4628466090",
+        SkyboxDn = "http://www.roblox.com/asset/?id=4628471901",
+        SkyboxFt = "http://www.roblox.com/asset/?id=4628466090",
+        SkyboxLf = "http://www.roblox.com/asset/?id=4628466090",
+        SkyboxRt = "http://www.roblox.com/asset/?id=4628466090",
+        SkyboxUp = "http://www.roblox.com/asset/?id=4628472152",
+    },
+
+    GreenNebula = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=47974894",
+        SkyboxDn = "http://www.roblox.com/asset/?id=47974690",
+        SkyboxFt = "http://www.roblox.com/asset/?id=47974821",
+        SkyboxLf = "http://www.roblox.com/asset/?id=47974776",
+        SkyboxRt = "http://www.roblox.com/asset/?id=47974859",
+        SkyboxUp = "http://www.roblox.com/asset/?id=47974909",
+    },
+
+    OrangeGradient = {
+        SkyboxBk = "rbxassetid://6902754982",
+        SkyboxDn = "rbxassetid://6902795826",
+        SkyboxFt = "rbxassetid://6902754982",
+        SkyboxLf = "rbxassetid://6902754982",
+        SkyboxRt = "rbxassetid://6902754982",
+        SkyboxUp = "rbxassetid://6902796078",
+    },
+
+    GreenAurora = {
+        SkyboxBk = "http://www.roblox.com/asset/?id=16563478983",
+        SkyboxDn = "http://www.roblox.com/asset/?id=16563481302",
+        SkyboxFt = "http://www.roblox.com/asset/?id=16563484084",
+        SkyboxLf = "http://www.roblox.com/asset/?id=16563485362",
+        SkyboxRt = "http://www.roblox.com/asset/?id=16563487078",
+        SkyboxUp = "http://www.roblox.com/asset/?id=16563489821",
+    },
 }
 
 return Skyboxes
